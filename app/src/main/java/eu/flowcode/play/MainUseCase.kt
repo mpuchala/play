fd3a5ebde61e8a13c4d5f9f19c8e254b98e75c13
mpuchala <puchala.michal@gmail.com>
2021-11-26 16:57:11 +0100

    Update app/src/main/java/eu/flowcode/play/MainUseCase.kt

--- app/src/main/java/eu/flowcode/play/MainUseCase.kt
+++ app/src/main/java/eu/flowcode/play/MainUseCase.kt
@@ -3,7 +3,7 @@ package eu.flowcode.play
 class MainUseCase {
     //We have another fix here
     fun execute(amount: Int): Int{
-        return amount + 12
+        return amount + 13
     }
 
     fun execute2(vv: Int){
