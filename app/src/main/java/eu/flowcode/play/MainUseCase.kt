13f36b465aa609257ed97470bb1354be7c04cf5f
mpuchala <puchala.michal@gmail.com>
2021-11-26 14:08:30 +0100

    Features/main use case fix (#5)
    * MainUseCase fix
    * MainUseCase fix another
    
    Squash

--- app/src/main/java/eu/flowcode/play/MainUseCase.kt
+++ app/src/main/java/eu/flowcode/play/MainUseCase.kt
@@ -1,8 +1,13 @@
 package eu.flowcode.play
 
 class MainUseCase {
-
+    //We have some fix here
     fun execute(amount: Int): Int{
-        return amount + 10
+        return amount + 11
+    }
+
+    //And next change
+    fun execute2(){
+
     }
 }
